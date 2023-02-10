eaea93a497bb784c2a08e219f16d46a0ea109cbf
Nico Michael <nic0michael@users.noreply.github.com>
2023-02-10 10:45:06 +0200

    Update CreateHA-ClusterDeploymentCommandsRedHatREL.sh

--- CreateHA-ClusterDeploymentCommandsRedHatREL.sh
+++ CreateHA-ClusterDeploymentCommandsRedHatREL.sh
@@ -1,6 +1,10 @@
 
 #!/bin/sh
 
+# IF YOU HAVE A PROBLEM CREATING THE OTHER NODES  AFTER CREATING THE MASTER NODE 
+# When installing my K3s worker, k3s fails to start due to "level=error msg="failed to get CA c>"
+# https://stackoverflow.com/questions/65872993/kubernetes-k3s-agent-cant-connect-to-master-ca-ssl-error
+
 ## 1. PERMANENTLY DISABLE FIREWALL ON MASTER AND OTHER SERVERS ##
 # Instructions can be found here
 # https://linuxhint.com/disable-firewall-centos-8/
@@ -8,10 +12,6 @@
 ## 2. INSTALL OPEN_SSH ON MASTER AND OTHER SERVERS ##
 # Instructions can be found here
 # https://linuxways.net/centos/how-to-enable-ssh-on-centos/
-
-# IF YOU HAVE A PROBLEM CREATING THE OTHER NODES  AFTER CREATING THE MASTER NODE 
-# When installing my K3s worker, k3s fails to start due to "level=error msg="failed to get CA c>"
-# https://stackoverflow.com/questions/65872993/kubernetes-k3s-agent-cant-connect-to-master-ca-ssl-error
 
 ## 3. INSTALL NANO ##
 echo "sudo yum install nano"
